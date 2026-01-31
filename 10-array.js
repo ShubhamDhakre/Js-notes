d4eb38548822e5429ae25140c3412f1e052d3f89
Shubham Dhakre <ssdhakre93@gmail.com>
2026-01-31 16:56:35 +0530

    5th

--- 10-array.js
+++ 10-array.js
@@ -25,4 +25,17 @@ console.log(myarray)
 console.log(myarray.indexOf(7))  //... it will give index of specific value
 console.log(myarray.includes(5))  //... it will give true if value is present otherwise false
 
+//...slice and splice methods
+
+  //..slice
+let newArray = myarray.slice(2,5)  //... it will give new array from index 2 to 4 (5-1)
+console.log(newArray)
+console.log(myarray)  //... original array will remain same
+
+  //..splice
+let splicedArray = myarray.splice(2,3)  //... it will remove 3 elements from index 2 and return removed elements
+console.log(splicedArray)
+console.log(myarray)  //... original array will be modified
+
+
  
